e90227f7af35c6e53e822ea6aecd6cc622537f4a
kazuki <kzk0802kazuki@gmail.com>
2019-09-04 20:55:56 +0900

    ABC139D問題AC解答

---
+++ abc139_d.cpp
@@ -0,0 +1,15 @@
+#include"bits/stdc++.h"
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	ll N;
+	cin >> N;
+	cout << N * (N - 1) / 2 << endl;
+
+	return 0;
+}
